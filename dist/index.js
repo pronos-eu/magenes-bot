287fefda9cd021a74b10b765f58f02e26623efc7
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-01-28 11:57:53 +0100

    Attempt at fixing context issues

--- dist/index.js
+++ dist/index.js
@@ -8313,9 +8313,9 @@ const loglistReviews = async (octokit, context) => {
     //core.info(JSON.stringify({ owner: context.owner, repo: context.repo, pull: context.pull_number }))
     core.info(JSON.stringify({ owner: context.owner, pull: context.pull_number }))
     const result = await octokit.rest.pulls.listReviews({
-        ...context.owner,
-        ...context.repo,
-        ...context.pull_number,
+        ...context.repo.owner,
+        ...context.repo.repo,
+        ...context.issue.number,
     })
     core.info(result);
 }
