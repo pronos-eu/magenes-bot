4300168846d19e46040513314bb0de3faeb510a6
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-01-28 12:34:52 +0100

    Added getting pull requests

--- dist/index.js
+++ dist/index.js
@@ -8311,16 +8311,11 @@ const core = __nccwpck_require__(2186);
 
 const loglistReviews = async (octokit, context) => {
     core.info(JSON.stringify({ owner: context.repo.owner, repo: context.repo.repo, pull: context.issue.number }))
-    // const result = await octokit.rest.pulls.listReviews({
-    //     ...context.repo.owner,
-    //     ...context.repo.repo,
-    //     ...context.issue.number,
-    // })
-    const result = await octokit.rest.issues.create({
-        ...context.repo,
-        title: 'New issue!',
-        body: 'Hello Universe!'
-    });
+    const result = await octokit.rest.pulls.get({
+        ...context.repo.owner,
+        ...context.repo.repo,
+        ...context.issue.number,
+    })
     core.info(result);
 }
 
--- plugins/label_approved.js
+++ plugins/label_approved.js
@@ -2,16 +2,11 @@ const core = require('@actions/core');
 
 export const loglistReviews = async (octokit, context) => {
     core.info(JSON.stringify({ owner: context.repo.owner, repo: context.repo.repo, pull: context.issue.number }))
-    // const result = await octokit.rest.pulls.listReviews({
-    //     ...context.repo.owner,
-    //     ...context.repo.repo,
-    //     ...context.issue.number,
-    // })
-    const result = await octokit.rest.issues.create({
-        ...context.repo,
-        title: 'New issue!',
-        body: 'Hello Universe!'
-    });
+    const result = await octokit.rest.pulls.get({
+        ...context.repo.owner,
+        ...context.repo.repo,
+        ...context.issue.number,
+    })
     core.info(result);
 }
 
