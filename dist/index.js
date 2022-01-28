dffb8c5fad9d7f55592342b7481579a330eb993c
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-01-28 12:37:44 +0100

    Fix for getting PR's

--- dist/index.js
+++ dist/index.js
@@ -8311,9 +8311,9 @@ const core = __nccwpck_require__(2186);
 
 const loglistReviews = async (octokit, context) => {
     core.info(JSON.stringify({ owner: context.repo.owner, repo: context.repo.repo, pull: context.issue.number }))
-    const result = await octokit.rest.pulls.get({
+    const result = await octokit.pulls.get({
         ...context.repo.owner,
-        ...context.repo.repo,
+        ...context.repo,
         ...context.issue.number,
     })
     core.info(result);
--- plugins/label_approved.js
+++ plugins/label_approved.js
@@ -2,9 +2,9 @@ const core = require('@actions/core');
 
 export const loglistReviews = async (octokit, context) => {
     core.info(JSON.stringify({ owner: context.repo.owner, repo: context.repo.repo, pull: context.issue.number }))
-    const result = await octokit.rest.pulls.get({
+    const result = await octokit.pulls.get({
         ...context.repo.owner,
-        ...context.repo.repo,
+        ...context.repo,
         ...context.issue.number,
     })
     core.info(result);
