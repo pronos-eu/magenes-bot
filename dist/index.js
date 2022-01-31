904c7212e9305ed86069f336f54f45c64074127b
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-01-31 17:09:01 +0100

    Improved list_of_reviews

--- dist/index.js
+++ dist/index.js
@@ -8325,12 +8325,10 @@ const parseReviews = (json_input) => {
     for (var i = 0; i < json_input.length; i++) {
         const reviewer = json_input[i].user.login;
         const state = json_input[i].state;
-        if (state == "APPROVED") {
-            list_of_reviews.push({ reviewer: reviewer })
+        if (state == "APPROVED" && list_of_reviews.includes(reviewer)) {
+            list_of_reviews.push(reviewer)
         }
-
     }
-    const unique_reviews = Array.from(new Set(list_of_reviews.map(({ reviewer }) => reviewer)));
     return unique_reviews
 }
 
