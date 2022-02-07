6a439c667272d4b1230d2d35513859c2a6b39002
Karol Kędzierski <75727403+karol-kedzierski@users.noreply.github.com>
2022-02-07 12:24:27 +0100

    V2 version (#6)
    Added displaying code coverage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const label_approved = require('./plugins/label_approved').default
+const show_code_coverage = require('./plugins/show_code_coverage').default
 
 async function run() {
   try {
@@ -8,11 +9,23 @@ async function run() {
     const octokit = github.getOctokit(myToken);
     const context = github.context;
     const labelerTrigger = core.getInput('labelerTrigger') === 'true';
+    const codeCoverageTrigger = core.getInput('codeCoverageTrigger') === 'true';
 
     if (labelerTrigger) {
       const numberOfApproves = parseInt(core.getInput('labelerApproves'));
       await label_approved(octokit, context, numberOfApproves);
     }
+
+    if (codeCoverageTrigger) {
+      const coverageStatements = core.getInput('coverageStatements');
+      const coverageBranches = core.getInput('coverageBranches');
+      const coverageFunctions = core.getInput('coverageFunctions');
+      const coverageLines = core.getInput('coverageLines');
+      core.info("COVERAGE REPORT: ")
+      const coverageReport = [coverageStatements, coverageBranches, coverageFunctions, coverageLines]
+      await show_code_coverage(octokit, context, coverageReport);
+    }
+
   } catch (error) {
     core.setFailed(error.message)
   }
---
+++ plugins/show_code_coverage.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+export const showCodeCoverage = async (octokit, context, coverageReport) => {
+    core.info(coverageReport);
+    createComment(octokit, context, coverageReport)
+}
+
+const createComment = async (octokit, context, coverageReport) => {
+    try {
+        await octokit.request('POST /repos/{owner}/{repo}/issues/{pull_number}/comments', {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: context.issue.number,
+            body: coverageReport[0] + "\n" +
+                coverageReport[1] + "\n" +
+                coverageReport[2] + "\n" +
+                coverageReport[3],
+        })
+    } catch (error) {
+        core.info(error.message)
+    }
+}
+export default showCodeCoverage
