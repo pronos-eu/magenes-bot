7cf01bafcbb24b40f61ddc970f252fe48b7bd4bc
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-02-07 16:14:03 +0100

    Fixed getModifiedFiles request method

--- dist/index.js
+++ dist/index.js
@@ -8378,17 +8378,16 @@ const core = __nccwpck_require__(2186);
 
 const lintModifiedFiles = async (octokit, context) => {
     modifiedFiles = getModifiedFiles(octokit, context);
-    // core.info(modifiedFiles);
+    core.info(modifiedFiles);
 }
 
 const getModifiedFiles = async (octokit, context) => {
     try {
-        const modifiedFiles = await octokit.request('/repos/{owner}/{repo}/pulls/{pull_number}/files', {
+        const modifiedFiles = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
             owner: context.repo.owner,
             repo: context.repo.repo,
             pull_number: context.issue.number,
         })
-        core.info(modifiedFiles);
         return modifiedFiles;
     } catch (error) {
         core.info(error.message);
