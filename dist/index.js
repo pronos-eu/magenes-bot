029bf4a8e9771e12c315093d0563ddab308eb22b
Karol Kędzierski <karol.kedzierski@pronos.eu>
2022-02-08 11:51:29 +0100

    Added parsing coverage report

--- dist/index.js
+++ dist/index.js
@@ -8419,13 +8419,24 @@ const core = __nccwpck_require__(2186);
 
 const showCodeCoverage = async (octokit, context, coverageReport) => {
     const parsedCoverageReport = parseCoverageReport(coverageReport);
+    core.info(parsedCoverageReport);
     // createComment(octokit, context, parsedCoverageReport)
 }
 
 const parseCoverageReport = (coverageReport) => {
     coverageReport = "{ " + coverageReport + " }";
     const parsedCoverageReport = JSON.parse(coverageReport);
-    core.info(JSON.stringify(parsedCoverageReport));
+    const statementsJson = parseCoverageReport.total.statements;
+    const branchesJson = parseCoverageReport.total.branches;
+    const functionsJson = parseCoverageReport.total.functions;
+    const linesJson = parseCoverageReport.total.lines;
+    const statements = "Statements: " + statementsJson.pct + "% " + "(" + statementsJson.covered + "/" + statementsJson.total + ")";
+    const branches = "Branches: " + branchesJson.pct + "% " + "(" + branchesJson.covered + "/" + branchesJson.total + ")";
+    const functions = "Functions: " + functionsJson.pct + "% " + "(" + functionsJson.covered + "/" + functionsJson.total + ")";
+    const lines = "Lines: " + linesJson.pct + "% " + "(" + linesJson.covered + "/" + linesJson.total + ")";
+    const topLine = "============== CODE COVERAGE ==============";
+    const bottomLine = "===========================================";
+    return [topLine, statements, branches, functions, lines, bottomLine]
 }
 
 const createComment = async (octokit, context, coverageReport) => {
